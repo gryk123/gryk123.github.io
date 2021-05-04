c6cdca3be2fff0dea5ad3d90dc59f50cab0f9601
gryk123 <39852821+gryk123@users.noreply.github.com>
2021-05-04 09:06:18 +0200

    init commit

---
+++ index.php
@@ -0,0 +1,84 @@
+<html>
+<head>
+	<title> Kantor Kryptoffwalut</title>
+	<meta charset="utf-8">
+	<link href="style.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+	<div id="container">
+	<div id="kalkulator" >
+		tekst<div id="formularz"> Skontaktuj się z nami
+		<form class="kwes-form" mode="test" action="https://kwes.io/api/foreign/forms/QekbSJt9rQj8hSUdjUX7">
+			<label for="name">Your Name</label><br>
+      <input type="text" name="name">
+       <div class="form-row">
+        <label for="email">Email</label><br>
+        <input type="email" name="email" required id="email" data-error-text="Wpisz poprawny email">
+    </div>
+ <div class="form-row">
+        <label for="message">Wiadomość</label><br>
+        <textarea name="message" required data-error-text="Musisz wypełnić pole" id="message"></textarea>
+    </div>
+      <button type="submit">Submit</button>
+ 
+</form>
+	</div>
+	</div>
+	<div id="kontakt">
+		rmajewski@kanga.exchange tel. 530-474-039
+	</div>
+	<div class="content">
+	<div id="b1" class="box" >
+		Bezpiecznie i anonimowo<br>
+		<div id="b11" class="boxx">
+			tekst w jednym boxie
+		</div>
+	</div>
+	<div id="b2" class="box">
+		Stacjonarnie oraz mobilnie<br>
+		<div id="b21" class="boxx">
+			Punkt stacjonarny znajduję się w katowicach ... Możliwość dojazdu do klienta do 50km od centrum miasta
+		</div>
+	</div>
+	<div id="b3" class="box">
+		Obsługiwane kryptowaluty<br>
+		<div id="b31" class="boxx">
+			<ul>
+			<li>BTC</li>
+			<li>ETH</li>
+			<li>LINK</li>
+			<li>USDC</li>
+			<li>USDT</li>
+			<li>KNG</li>
+			<li>oPLN</li>
+			<li>oEUR</li>
+		</ul>
+		</div>
+	</div>	
+	<div id="b4" class="box">
+		szybko i wygodnie<br>
+		<div id="b41" class="boxx">
+			tekst w jednym boxie
+		</div>
+	</div>
+	<div id="b5" class="box">
+		Wiedza i doświadczenie<br>
+		<div id="b51" class="boxx">
+			tekst w jednym boxie
+		</div>
+	</div>
+	<div id="b6" class="box">
+		tytuł<br>
+		<div id="b61" class="boxx">
+			tekst w jednym boxie
+		</div>
+	</div>
+	<br>
+</div>
+	</div>
+	<div id="push"></div>
+	<div id="stopka">
+		stopka
+	</div>
+	<script src="https://kwes.io/v2/kwes-script.js" defer></script>
+</body>
